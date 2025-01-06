67780cede8f9df3be2100b472c1828ec0535749d
Luiz Alves <115586109+luizz-dev@users.noreply.github.com>
2025-01-06 17:19:05 -0300

    modal pronto

--- WebVision-main/WebVision/js/script.js
+++ WebVision-main/WebVision/js/script.js
@@ -22,3 +22,48 @@ function openContato() {
 function openServicos() {
   window.location.href = "http://127.0.0.1:5500/WebVision/index.html#servicos";
 }
+
+// modal
+const botaoFechar = document.querySelector('[data-modal="fechar"]');
+const botaoAbir = document.querySelector('[ data-modal="abrir"]');
+const containerModal = document.querySelector('[ data-modal="container"]');
+
+const botaoFecharC = document.querySelector('[data-modal="fechar-cadastro"]');
+
+const containerModalC = document.querySelector(
+  '[data-modal="container-cadastro"]'
+);
+
+const cadastroBtn = document.querySelector('[ data-modal="btn-cadastro"]');
+const loginBtn = document.querySelector('[ data-modal="btn-login"]');
+
+function abrirModal(event) {
+  event.preventDefault();
+  containerModal.classList.add("ativo");
+}
+function fecharModal(event) {
+  event.preventDefault();
+  containerModal.classList.remove("ativo");
+}
+
+function abrirModalC(event) {
+  event.preventDefault();
+  fecharModal(event);
+  containerModalC.classList.add("ativo");
+}
+function fecharModalC(event) {
+  event.preventDefault();
+  containerModalC.classList.remove("ativo");
+}
+function abrirModalBtn(event) {
+  event.preventDefault(event);
+  containerModal.classList.add("ativo");
+  fecharModalC(event);
+}
+
+botaoAbir.addEventListener("click", abrirModal);
+botaoFechar.addEventListener("click", fecharModal);
+
+botaoFecharC.addEventListener("click", fecharModalC);
+cadastroBtn.addEventListener("click", abrirModalC);
+loginBtn.addEventListener("click", abrirModalBtn);
